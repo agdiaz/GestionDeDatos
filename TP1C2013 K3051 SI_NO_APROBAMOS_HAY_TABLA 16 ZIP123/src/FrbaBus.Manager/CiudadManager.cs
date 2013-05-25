5ca197b6d3adf131bffeaac052502eb76b9f7156
rojascristian <rojas.r.cristian@gmail.com>
2013-05-25 18:48:54 -0300

    Se agrega la estructura para manipular los datos

---
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus.Manager/CiudadManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrbaBus.DAO;
+using FrbaBus.Common.Entidades;
+
+namespace FrbaBus.Manager
+{
+    public class CiudadManager
+    {
+        public void Alta(string descripcion)
+        {
+            CiudadDAO ciudadDAO = new CiudadDAO();
+            Ciudad ciudad = new Ciudad() { Descripcion = descripcion };
+            ciudadDAO.Alta(ciudad);
+        }
+    }
+}
