23ee3ca0356cb46ab946ad74c30d3ec74cd6f025
demodawid <demodawid@gmail.com>
2013-06-13 21:15:44 -0300

    Script recorridos completo

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Recorrido.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Recorrido.sql
@@ -1,3 +1,53 @@
+USE [GD1C2013]
+GO
+
+/****** Object:  Table [SI_NO_APROBAMOS_HAY_TABLA].[Recorrido]    Script Date: 06/13/2013 21:13:26 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Recorrido](
+	[id_recorrido] [numeric](18, 0) NOT NULL,
+	[id_ciudad_origen] [int] NOT NULL,
+	[id_ciudad_destino] [int] NOT NULL,
+	[pre_base_kg] [numeric](18, 2) NOT NULL,
+	[pre_base_pasaje] [numeric](18, 2) NOT NULL,
+	[id_servicio] [int] NOT NULL,
+	[baja] [bit] NOT NULL,
+ CONSTRAINT [PK_Recorrido] PRIMARY KEY CLUSTERED 
+(
+	[id_recorrido] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Recorrido]  WITH CHECK ADD  CONSTRAINT [FK_Recorrido_Destino] FOREIGN KEY([id_ciudad_destino])
+REFERENCES [SI_NO_APROBAMOS_HAY_TABLA].[Ciudad] ([id_ciudad])
+GO
+
+ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Recorrido] CHECK CONSTRAINT [FK_Recorrido_Destino]
+GO
+
+ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Recorrido]  WITH CHECK ADD  CONSTRAINT [FK_Recorrido_Origen] FOREIGN KEY([id_ciudad_origen])
+REFERENCES [SI_NO_APROBAMOS_HAY_TABLA].[Ciudad] ([id_ciudad])
+GO
+
+ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Recorrido] CHECK CONSTRAINT [FK_Recorrido_Origen]
+GO
+
+ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Recorrido]  WITH CHECK ADD  CONSTRAINT [FK_Recorrido_Servicio] FOREIGN KEY([id_servicio])
+REFERENCES [SI_NO_APROBAMOS_HAY_TABLA].[Servicio] ([id_servicio])
+GO
+
+ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Recorrido] CHECK CONSTRAINT [FK_Recorrido_Servicio]
+GO
+
+ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Recorrido] ADD  CONSTRAINT [DF_Recorrido_baja]  DEFAULT ((0)) FOR [baja]
+GO
+
 select	m2.Recorrido_Codigo					as id_recorrido, 
 		corig.id_ciudad						as id_ciudad_origen,
 		cdes.id_ciudad						as id_ciudad_destino,
