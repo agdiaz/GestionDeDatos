949349a1996992608f027cae10667299752cfaa5
demodawid <demodawid@gmail.com>
2013-06-13 22:57:32 -0300

    Agregado de INSERT en recorrido

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Recorrido.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Recorrido.sql
@@ -48,22 +48,25 @@ GO
 ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Recorrido] ADD  CONSTRAINT [DF_Recorrido_baja]  DEFAULT ((0)) FOR [baja]
 GO
 
-select	m2.Recorrido_Codigo					as id_recorrido, 
-		corig.id_ciudad						as id_ciudad_origen,
-		cdes.id_ciudad						as id_ciudad_destino,
-		SUM(m2.Recorrido_Precio_BaseKG)		as pre_base_kg, 
-		SUM(m2.Recorrido_Precio_BasePasaje)	as pre_base_pasaje,
-		serv.id_servicio					as id_servicio
-from( 
-	select distinct m.Recorrido_Codigo, m.Recorrido_Ciudad_Origen,
-					m.Recorrido_Ciudad_Destino,	m.Recorrido_Precio_BaseKG, 
-					m.Recorrido_Precio_BasePasaje, m.Tipo_Servicio
-	from gd_esquema.Maestra m
-) as m2
-inner join SI_NO_APROBAMOS_HAY_TABLA.Ciudad cdes
-	on cdes.nombre = m2.Recorrido_Ciudad_Destino
-inner join SI_NO_APROBAMOS_HAY_TABLA.Ciudad corig
-	on corig.nombre = m2.Recorrido_Ciudad_Origen
-inner join SI_NO_APROBAMOS_HAY_TABLA.Servicio serv
-	on serv.tipo_servicio = m2.Tipo_Servicio
-group by m2.Recorrido_Codigo, corig.id_ciudad, cdes.id_ciudad, serv.id_servicio
+insert into SI_NO_APROBAMOS_HAY_TABLA.Recorrido (id_recorrido, id_ciudad_origen, id_ciudad_destino, pre_base_kg, pre_base_pasaje, id_servicio)
+(
+	select	m2.Recorrido_Codigo					as id_recorrido, 
+			corig.id_ciudad						as id_ciudad_origen,
+			cdes.id_ciudad						as id_ciudad_destino,
+			SUM(m2.Recorrido_Precio_BaseKG)		as pre_base_kg, 
+			SUM(m2.Recorrido_Precio_BasePasaje)	as pre_base_pasaje,
+			serv.id_servicio					as id_servicio
+	from( 
+		select distinct m.Recorrido_Codigo, m.Recorrido_Ciudad_Origen,
+						m.Recorrido_Ciudad_Destino,	m.Recorrido_Precio_BaseKG, 
+						m.Recorrido_Precio_BasePasaje, m.Tipo_Servicio
+		from gd_esquema.Maestra m
+	) as m2
+	inner join SI_NO_APROBAMOS_HAY_TABLA.Ciudad cdes
+		on cdes.nombre = m2.Recorrido_Ciudad_Destino
+	inner join SI_NO_APROBAMOS_HAY_TABLA.Ciudad corig
+		on corig.nombre = m2.Recorrido_Ciudad_Origen
+	inner join SI_NO_APROBAMOS_HAY_TABLA.Servicio serv
+		on serv.tipo_servicio = m2.Tipo_Servicio
+	group by m2.Recorrido_Codigo, corig.id_ciudad, cdes.id_ciudad, serv.id_servicio
+)
