edd297ab8ff8b6977a698a997a1bfc39b1a99759
demodawid <demodawid@gmail.com>
2013-06-13 23:32:40 -0300

    Script de micros corregido

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Micros.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Micros.sql
@@ -1,7 +1,7 @@
 USE [GD1C2013]
 GO
 
-/****** Object:  Table [SI_NO_APROBAMOS_HAY_TABLA].[Micros]    Script Date: 06/13/2013 21:51:53 ******/
+/****** Object:  Table [SI_NO_APROBAMOS_HAY_TABLA].[Micros]    Script Date: 06/13/2013 23:30:38 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,7 +10,7 @@ GO
 
 CREATE TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros](
 	[id_micros] [int] IDENTITY(1,1) NOT NULL,
-	[fecha_alta] [datetime] NOT NULL,
+	[fecha_alta] [datetime2](7) NOT NULL,
 	[nro_micro] [int] NULL,
 	[modelo] [nvarchar](50) NOT NULL,
 	[patente] [nvarchar](50) NOT NULL,
@@ -31,18 +31,21 @@ CREATE TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros](
 
 GO
 
-ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros]  WITH CHECK ADD  CONSTRAINT [FK_Micros_Marcas] FOREIGN KEY([id_micros])
+ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros]  WITH CHECK ADD  CONSTRAINT [FK_Micros_Marcas] FOREIGN KEY([id_marca])
 REFERENCES [SI_NO_APROBAMOS_HAY_TABLA].[Marca] ([id_marca])
 GO
 
 ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros] CHECK CONSTRAINT [FK_Micros_Marcas]
 GO
 
-ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros]  WITH CHECK ADD  CONSTRAINT [FK_Micros_Servicios] FOREIGN KEY([id_micros])
+ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros]  WITH CHECK ADD  CONSTRAINT [FK_Micros_Servicios] FOREIGN KEY([id_servicio])
 REFERENCES [SI_NO_APROBAMOS_HAY_TABLA].[Servicio] ([id_servicio])
 GO
 
 ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros] CHECK CONSTRAINT [FK_Micros_Servicios]
+GO
+
+ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros] ADD  CONSTRAINT [DF_Micros_fecha_alta]  DEFAULT (getdate()) FOR [fecha_alta]
 GO
 
 ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros] ADD  CONSTRAINT [DF_Micros_baja_fuera_servicio]  DEFAULT ((0)) FOR [baja_fuera_servicio]
@@ -54,21 +57,23 @@ GO
 ALTER TABLE [SI_NO_APROBAMOS_HAY_TABLA].[Micros] ADD  CONSTRAINT [DF_Micros_baja]  DEFAULT ((0)) FOR [baja]
 GO
 
-
-
-select m2.Micro_Modelo			as modelo,
-		m2.Micro_Patente		as patente,
-		mar.id_marca			as id_marca,
-		serv.id_servicio		as id_servicio,
-		m2.Micro_KG_Disponibles as capacidad_kg
-from
+insert into SI_NO_APROBAMOS_HAY_TABLA.Micros
+	(modelo, patente, id_marca, id_servicio, capacidad_kg)
 (
-	select distinct m.Micro_Modelo, m.Micro_Patente, 
-				m.Micro_Marca, m.Tipo_Servicio,
-				m.Micro_KG_Disponibles
-	from gd_esquema.Maestra m
-) as m2
-inner join SI_NO_APROBAMOS_HAY_TABLA.Marca mar
-	on mar.nombre = m2.Micro_Marca
-inner join SI_NO_APROBAMOS_HAY_TABLA.Servicio serv
-	on serv.tipo_servicio = m2.Tipo_Servicio
+	select m2.Micro_Modelo			as modelo,
+			m2.Micro_Patente		as patente,
+			mar.id_marca			as id_marca,
+			serv.id_servicio		as id_servicio,
+			m2.Micro_KG_Disponibles as capacidad_kg
+	from
+	(
+		select distinct m.Micro_Modelo, m.Micro_Patente, 
+					m.Micro_Marca, m.Tipo_Servicio,
+					m.Micro_KG_Disponibles
+		from gd_esquema.Maestra m
+	) as m2
+	inner join SI_NO_APROBAMOS_HAY_TABLA.Marca mar
+		on mar.nombre = m2.Micro_Marca
+	inner join SI_NO_APROBAMOS_HAY_TABLA.Servicio serv
+		on serv.tipo_servicio = m2.Tipo_Servicio
+)
