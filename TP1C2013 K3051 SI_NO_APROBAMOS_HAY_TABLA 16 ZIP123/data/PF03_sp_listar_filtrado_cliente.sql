ae16e19cbf140f6f0b6b89445b55c8ce212ed4b3
Flor <florxgomez@gmail.com>
2013-06-20 20:49:10 -0300

    FILTRADO CLIENTE

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/PF03_sp_listar_filtrado_cliente.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/PF03_sp_listar_filtrado_cliente.sql
@@ -1,4 +1,7 @@
-CREATE PROCEDURE [SI_NO_APROBAMOS_HAY_TABLA].sp_listar_cliente
+CREATE PROCEDURE [SI_NO_APROBAMOS_HAY_TABLA].sp_listar_filtrado_cliente
+	@p_dni numeric(18,0) = NULL,
+	@p_nombre nvarchar(255) = NULL,
+	@p_apellido nvarchar(255) = NULL
 AS
 BEGIN
 	SELECT [dni]
@@ -10,5 +13,8 @@ BEGIN
       ,[fecha_nacimiento]
       ,[es_discapacitado]
       ,[sexo]
-  FROM [GD1C2013].[SI_NO_APROBAMOS_HAY_TABLA].[Cliente]
+	FROM [GD1C2013].[SI_NO_APROBAMOS_HAY_TABLA].[Cliente]
+	where ((@p_dni IS NULL) OR (@p_dni = dni))
+	and ((@p_nombre IS NULL) OR (@p_nombre = nombre))
+	and ((@p_apellido IS NULL) OR (@p_apellido = apellido))
 END
