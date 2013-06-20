aa4982b3954b784b119eb798b67482682f7910d9
demodawid <demodawid@gmail.com>
2013-06-20 13:11:53 -0300

    insertar admin

---
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/procedures/insertarAdminNOUSAR.sql
@@ -0,0 +1,32 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Demian
+-- Create date: 10/6/2013
+-- =============================================
+CREATE PROCEDURE [SI_NO_APROBAMOS_HAY_TABLA].insertar_usuario_admin
+	@userName nvarchar(50),
+	@passwordHash	varbinary(32)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	DECLARE @idAdmin int
+
+	SELECT @idAdmin = r.id_rol
+	FROM SI_NO_APROBAMOS_HAY_TABLA.Rol r
+	WHERE r.nombre = 'Administrativo'
+	
+	DECLARE @dniLoco numeric(18,0)
+	
+	SELECT top 1 @dniLoco = c.dni
+	FROM SI_NO_APROBAMOS_HAY_TABLA.Cliente c
+	
+	INSERT INTO SI_NO_APROBAMOS_HAY_TABLA.Usuario
+	(id_rol, dni, username, hash_password)
+	VALUES
+	(@idAdmin, @dniLoco, @userName, @passwordHash)
+END
+GO
