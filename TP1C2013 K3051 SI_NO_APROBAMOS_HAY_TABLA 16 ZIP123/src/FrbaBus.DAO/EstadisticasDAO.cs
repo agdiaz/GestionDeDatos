20dfa6b845ac44f6f40970fc2159704bd3ff5bc6
Adrián Diaz <diaz.adrian.g@gmail.com>
2013-06-20 13:50:59 -0300

    Managers y DAOs. Para estadisticas ya estan definidios los nombres de las vistas.

---
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus.DAO/EstadisticasDAO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GestionDeDatos.AccesoDatos;
+using System.Data;
+
+namespace FrbaBus.DAO
+{
+    public class EstadisticasDAO
+    {
+        private IAccesoBD accesoBD = new AccesoBD();
+
+        public DataSet DestinosMasVendidos()
+        {
+            string consulta = "SELECT * FROM [SI_NO_APROBAMOS_HAY_TABLA].[v_DestinosMasVendidos]";
+            return this.accesoBD.RealizarConsulta(consulta);
+        }
+
+        public DataSet DestinosConMasMicrosVacios()
+        {
+            string consulta = "SELECT * FROM [SI_NO_APROBAMOS_HAY_TABLA].[v_DestinosConMasMicrosVacios]";
+            return this.accesoBD.RealizarConsulta(consulta);
+        }
+
+        public DataSet ClientesConMasPuntos()
+        {
+            string consulta = "SELECT * FROM [SI_NO_APROBAMOS_HAY_TABLA].[v_ClientesConMasPuntos]";
+            return this.accesoBD.RealizarConsulta(consulta);
+        }
+
+        public DataSet DestinosMasCancelados()
+        {
+            string consulta = "SELECT * FROM [SI_NO_APROBAMOS_HAY_TABLA].[v_DestinosMasCancelados]";
+            return this.accesoBD.RealizarConsulta(consulta);
+        }
+
+        public DataSet MicrosConMasDiasFueraDeServicio()
+        {
+            string consulta = "SELECT * FROM [SI_NO_APROBAMOS_HAY_TABLA].[v_MicrosConMasDiasFueraDeServicio]";
+            return this.accesoBD.RealizarConsulta(consulta);
+        }
+
+    }
+}
---
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus.Manager/EstadisticasManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace FrbaBus.Manager
+{
+    public class EstadisticasManager
+    {
+        public DataSet DestinosMasVendidos()
+        {
+            return new DataSet();
+        }
+
+        public DataSet DestinosConMasMicrosVacios()
+        {
+            return new DataSet();
+        }
+
+        public DataSet ClientesConMasPuntos()
+        {
+            return new DataSet();
+        }
+
+        public DataSet DestinosMasCancelados()
+        {
+            return new DataSet();
+        }
+
+        public DataSet MicrosConMasDiasFueraDeServicio()
+        {
+            return new DataSet();
+        }
+    }
+}
