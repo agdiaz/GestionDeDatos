3e4f815a5faaceb745432fd479419724dd8482fa
Adrián Diaz <diaz.adrian.g@gmail.com>
2013-07-15 20:06:56 -0300

    Nuevos DAO

---
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus.DAO/Builder/CanjeBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrbaBus.Common.Entidades;
+
+namespace FrbaBus.DAO.Builder
+{
+    public class CanjeBuilder : IBuilder<Canje>
+    {
+        #region Miembros de IBuilder<Canje>
+
+        public Canje Build(System.Data.DataRow row)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus.DAO/RecompensaDAO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrbaBus.Common.Entidades;
+
+namespace FrbaBus.DAO
+{
+    public class RecompensaDAO : IEntidadDAO<Recompensa>
+    {
+
+        #region Miembros de IEntidadDAO<Recompensa>
+
+        public GestionDeDatos.AccesoDatos.IAccesoBD accesoBD
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Recompensa Obtener(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Alta(Recompensa entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Baja(Recompensa entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modificacion(Recompensa entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Recompensa> Listar()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
