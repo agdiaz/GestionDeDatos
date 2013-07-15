0d6bc32941f79377b70127a1d87cd81c7330e7fc
demodawid <demodawid@gmail.com>
2013-07-15 19:09:19 -0300

    Mas SPs

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/script_creacion_inicial.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/script_creacion_inicial.sql
@@ -1724,6 +1724,21 @@ BEGIN
 END
 GO
 
+/*===========================SP Listar puntos por cliente==============================*/
+CREATE PROCEDURE [SI_NO_APROBAMOS_HAY_TABLA].[sp_listar_puntos_por_cliente]
+(
+	@p_dni numeric(18,0)
+)
+AS
+BEGIN
+	SELECT p.dni, SUM(p.puntos - p.puntos_usados) as 'puntosTotales'
+	FROM SI_NO_APROBAMOS_HAY_TABLA.Puntaje p
+	WHERE DATEDIFF(year,p.fecha_otorgado, GETDATE() ) < 1
+	AND p.dni = @p_dni
+	GROUP BY p.dni
+	ORDER BY puntosTotales
+END
+
 /*===========================SP LISTAR CIUDAD==============================*/
 
 
