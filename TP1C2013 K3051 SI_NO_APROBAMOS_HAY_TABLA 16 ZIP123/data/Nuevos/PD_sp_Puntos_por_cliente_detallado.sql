3b9f224ab95929bbbfa24d9946fcae87a1401bc1
demodawid <demodawid@gmail.com>
2013-07-18 18:13:43 -0300

    Mas cosas de puntajes

---
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Nuevos/PD_sp_Puntos_por_cliente_detallado.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE [SI_NO_APROBAMOS_HAY_TABLA].sp_puntos_por_cliente_detallado
+(
+	@p_dni numeric(18,0)
+)
+AS
+BEGIN
+	SELECT p.dni,
+	(p.puntos) as 'puntosOtorgados',
+	(p.puntos_usados) as 'puntosUtilizados', 
+	(p.puntos - p.puntos_usados) as 'puntosRestantes',
+	(p.fecha_otorgado)
+	FROM SI_NO_APROBAMOS_HAY_TABLA.Puntaje p
+	WHERE DATEDIFF(year,p.fecha_otorgado, GETDATE() ) < 1
+	AND p.dni = 1208119
+	ORDER BY puntosRestantes DESC
+END
