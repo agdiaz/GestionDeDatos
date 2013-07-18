3b680a3fe162dd6fd41acf15ccba83f1bdece04c
rojascristian <rojas.r.cristian@gmail.com>
2013-07-18 16:17:12 -0300

    Se arregla la estadística clientes con más puntos

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/SPs_estadisticas/sp_clientes_mas_puntos.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/SPs_estadisticas/sp_clientes_mas_puntos.sql
@@ -7,8 +7,7 @@ AS
 BEGIN
 	SELECT TOP 5 p.dni, SUM(p.puntos - p.puntos_usados) as 'puntosTotales'
 	FROM SI_NO_APROBAMOS_HAY_TABLA.Puntaje p
-	WHERE DATEDIFF(year,p.fecha_otorgado, GETDATE() ) < 1
-	AND p.fecha_otorgado BETWEEN @fecha_inicio AND @fecha_fin
+	WHERE p.fecha_otorgado BETWEEN @fecha_inicio AND @fecha_fin
 	GROUP BY p.dni
 	ORDER BY puntosTotales DESC
 END
