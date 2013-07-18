b7515422b3fcca42c0c0f565af2b7f14396f2ff5
rojascristian <rojas.r.cristian@gmail.com>
2013-07-18 15:03:48 -0300

    Se agregan las estadísticas para
    - micros con más días fuera de servicio
    - clientes con más puntos

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus.Common/Entidades/Estadisticas/ClienteConMasPuntos.cs
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus.Common/Entidades/Estadisticas/ClienteConMasPuntos.cs
@@ -7,5 +7,7 @@ namespace FrbaBus.Common.Entidades.Estadisticas
 {
     public class ClienteConMasPuntos : IListadoEstadistico
     {
+        public int Dni { get; set; }
+        public int PuntosTotales { get; set; }
     }
 }
