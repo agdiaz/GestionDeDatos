1111b208ab985adefe5cd1a1ec4c911d75b37050
Adrián Diaz <diaz.adrian.g@gmail.com>
2013-07-18 20:35:52 -0300

    abordar viaje

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus/Abm Viaje/ViajeCargarArribo.Designer.cs
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus/Abm Viaje/ViajeCargarArribo.Designer.cs
@@ -28,11 +28,224 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbMicro = new System.Windows.Forms.TextBox();
+            this.tbCiudadDestino = new System.Windows.Forms.TextBox();
+            this.tbCiudadOrigen = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMicro = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dtpFechaArriboReal = new System.Windows.Forms.DateTimePicker();
+            this.dtpFechaArriboEstimada = new System.Windows.Forms.DateTimePicker();
+            this.dtpFechaSalida = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbMicro);
+            this.groupBox1.Controls.Add(this.tbCiudadDestino);
+            this.groupBox1.Controls.Add(this.tbCiudadOrigen);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnMicro);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(463, 129);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Viaje";
+            // 
+            // tbMicro
+            // 
+            this.tbMicro.Enabled = false;
+            this.tbMicro.Location = new System.Drawing.Point(185, 87);
+            this.tbMicro.Name = "tbMicro";
+            this.tbMicro.Size = new System.Drawing.Size(264, 20);
+            this.tbMicro.TabIndex = 6;
+            // 
+            // tbCiudadDestino
+            // 
+            this.tbCiudadDestino.Enabled = false;
+            this.tbCiudadDestino.Location = new System.Drawing.Point(185, 61);
+            this.tbCiudadDestino.Name = "tbCiudadDestino";
+            this.tbCiudadDestino.Size = new System.Drawing.Size(264, 20);
+            this.tbCiudadDestino.TabIndex = 5;
+            // 
+            // tbCiudadOrigen
+            // 
+            this.tbCiudadOrigen.Enabled = false;
+            this.tbCiudadOrigen.Location = new System.Drawing.Point(185, 35);
+            this.tbCiudadOrigen.Name = "tbCiudadOrigen";
+            this.tbCiudadOrigen.Size = new System.Drawing.Size(264, 20);
+            this.tbCiudadOrigen.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Micro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ciudad destino";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ciudad origen";
+            // 
+            // btnMicro
+            // 
+            this.btnMicro.Location = new System.Drawing.Point(10, 35);
+            this.btnMicro.Name = "btnMicro";
+            this.btnMicro.Size = new System.Drawing.Size(75, 46);
+            this.btnMicro.TabIndex = 0;
+            this.btnMicro.Text = "Buscar Micro";
+            this.btnMicro.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dtpFechaArriboReal);
+            this.groupBox2.Controls.Add(this.dtpFechaArriboEstimada);
+            this.groupBox2.Controls.Add(this.dtpFechaSalida);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(13, 148);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(463, 110);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Arribo";
+            // 
+            // dtpFechaArriboReal
+            // 
+            this.dtpFechaArriboReal.Location = new System.Drawing.Point(139, 72);
+            this.dtpFechaArriboReal.Name = "dtpFechaArriboReal";
+            this.dtpFechaArriboReal.Size = new System.Drawing.Size(310, 20);
+            this.dtpFechaArriboReal.TabIndex = 5;
+            // 
+            // dtpFechaArriboEstimada
+            // 
+            this.dtpFechaArriboEstimada.Enabled = false;
+            this.dtpFechaArriboEstimada.Location = new System.Drawing.Point(139, 46);
+            this.dtpFechaArriboEstimada.Name = "dtpFechaArriboEstimada";
+            this.dtpFechaArriboEstimada.Size = new System.Drawing.Size(310, 20);
+            this.dtpFechaArriboEstimada.TabIndex = 4;
+            // 
+            // dtpFechaSalida
+            // 
+            this.dtpFechaSalida.Enabled = false;
+            this.dtpFechaSalida.Location = new System.Drawing.Point(138, 19);
+            this.dtpFechaSalida.Name = "dtpFechaSalida";
+            this.dtpFechaSalida.Size = new System.Drawing.Size(311, 20);
+            this.dtpFechaSalida.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Fecha de arribo real";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Fecha de arribo estimada";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Fecha de salida";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(314, 264);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(401, 264);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // ViajeCargarArribo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "ViajeCargarArribo";
+            this.ClientSize = new System.Drawing.Size(493, 307);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ViajeCargarArribo";
+            this.Text = "FrbaBus - Viaje :: Cargar arribo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox tbCiudadOrigen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMicro;
+        private System.Windows.Forms.TextBox tbMicro;
+        private System.Windows.Forms.TextBox tbCiudadDestino;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpFechaArriboReal;
+        private System.Windows.Forms.DateTimePicker dtpFechaArriboEstimada;
+        private System.Windows.Forms.DateTimePicker dtpFechaSalida;
+        private System.Windows.Forms.Label label6;
     }
 }
