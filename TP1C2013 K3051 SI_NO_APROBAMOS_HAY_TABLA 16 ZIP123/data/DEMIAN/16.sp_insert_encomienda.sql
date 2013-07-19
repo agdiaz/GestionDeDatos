c991a20e54cda6ac16e82bf7bc457fd9e37540db
demodawid <demodawid@gmail.com>
2013-07-19 20:27:13 -0300

    insert encomienda

---
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/DEMIAN/16.sp_insert_encomienda.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE SI_NO_APROBAMOS_HAY_TABLA.sp_insertar_encomienda
+(
+	@p_id_compra int,
+	@p_dni numeric(18,0),
+	@p_pre_encomienda int,
+	@p_id_viaje int
+)
+AS
+BEGIN
+	INSERT INTO SI_NO_APROBAMOS_HAY_TABLA.Encomienda
+		(id_viaje, id_compra, dni, pre_encomienda)
+	VALUES
+		(@p_id_viaje,@p_id_compra, @p_dni, @p_pre_encomienda)
+END
