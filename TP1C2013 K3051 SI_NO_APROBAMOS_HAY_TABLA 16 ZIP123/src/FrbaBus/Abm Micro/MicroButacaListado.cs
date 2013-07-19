0f474f6679097112b058d392e067aa33b18499dd
Adrián Diaz <diaz.adrian.g@gmail.com>
2013-07-18 23:30:10 -0300

    Listado de butaca

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus/Abm Micro/MicroButacaListado.cs
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus/Abm Micro/MicroButacaListado.cs
@@ -6,14 +6,55 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using FrbaBus.Common.Entidades;
+using FrbaBus.Manager;
 
 namespace FrbaBus.Abm_Micro
 {
     public partial class MicroButacaListado : Form
     {
+        private Micro _micro;
+        private ButacaManager _manager;
+
         public MicroButacaListado()
         {
+            _manager = new ButacaManager();
             InitializeComponent();
         }
+
+        public MicroButacaListado(Micro m)
+        :this()
+        {
+            _micro = m;
+        }
+        private void MicroButacaListado_Load(object sender, EventArgs e)
+        {
+            MostrarDatosMicro();
+            CargarListaTipoButaca();
+            CargarButacas();
+        }
+
+        private void CargarButacas()
+        {
+            
+        }
+
+        private void CargarListaTipoButaca()
+        {
+            IList<string> tipos = new List<string>();
+            tipos.Add(TipoButaca.Pasillo);
+            tipos.Add(TipoButaca.Ventanilla);
+
+            cbTipoButaca.DataSource = tipos;
+        }
+        private void MostrarDatosMicro()
+        {
+            if (_micro != null)
+            {
+                tbMicro.Text = _micro.Informacion;
+            }
+        }
+
+        
     }
 }
