1ea2c581e408a90be32e79e86dc92f6d8286681b
demodawid <demodawid@gmail.com>
2013-07-20 19:43:02 -0300

    pito culo teta

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Finales/01 - SELECT/34_sp_baja_logica_micro.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Finales/01 - SELECT/34_sp_baja_logica_micro.sql
@@ -6,25 +6,23 @@ AS
 BEGIN
 	SET xact_abort on
 	BEGIN TRANSACTION baja_logica_micro
-				
+	
 	DECLARE @idCompraAct int
 
 	DECLARE cur_compras CURSOR LOCAL for
-	SELECT DISTINCT c.id_compra
-	FROM SI_NO_APROBAMOS_HAY_TABLA.Viaje v
-	INNER JOIN SI_NO_APROBAMOS_HAY_TABLA.Pasaje p
-		ON p.id_viaje = v.id_viaje
-	INNER JOIN SI_NO_APROBAMOS_HAY_TABLA.Encomienda e
-		ON e.id_viaje = v.id_viaje
-	INNER JOIN SI_NO_APROBAMOS_HAY_TABLA.Compra c
-		ON p.id_compra = c.id_compra
-		OR e.id_compra = c.id_compra
-	WHERE v.id_micro = @id_micro
-	AND v.baja = 0
-	AND p.baja = 0
-	AND p.fecha_cancel is null
-	AND e.baja = 0
-	AND e.fecha_cancel is null
+		SELECT DISTINCT c.id_compra
+		FROM SI_NO_APROBAMOS_HAY_TABLA.Viaje v
+		INNER JOIN SI_NO_APROBAMOS_HAY_TABLA.Pasaje p
+			ON p.id_viaje = v.id_viaje
+		INNER JOIN SI_NO_APROBAMOS_HAY_TABLA.Encomienda e
+			ON e.id_viaje = v.id_viaje
+		INNER JOIN SI_NO_APROBAMOS_HAY_TABLA.Compra c
+			ON p.id_compra = c.id_compra
+			OR e.id_compra = c.id_compra
+		WHERE v.id_micro = @id_micro
+		AND v.baja = 0
+		AND p.baja = 0
+		AND e.baja = 0
 
 	open cur_compras
 
@@ -36,16 +34,13 @@ BEGIN
 		fetch next from cur_compras into @idCompraAct 
 	END
 
-		
-		
-		
-		UPDATE [GD1C2013].[SI_NO_APROBAMOS_HAY_TABLA].[Micros]
-		SET Micros.baja = 1
-		WHERE Micros.id_micros = @id_micro
-		
-		UPDATE [GD1C2013].[SI_NO_APROBAMOS_HAY_TABLA].[Butaca]
-		SET Butaca.baja = 1
-		WHERE Butaca.id_micro = @id_micro
+	UPDATE [GD1C2013].[SI_NO_APROBAMOS_HAY_TABLA].[Micros]
+	SET Micros.baja = 1
+	WHERE Micros.id_micros = @id_micro
+	
+	UPDATE [GD1C2013].[SI_NO_APROBAMOS_HAY_TABLA].[Butaca]
+	SET Butaca.baja = 1
+	WHERE Butaca.id_micro = @id_micro
 		
 	COMMIT TRANSACTION baja_logica_micro
 END
