a111649be953e4c7d6be439cb99e9b0e92c08a88
demodawid <demodawid@gmail.com>
2013-07-20 01:37:35 -0300

    fuck shit piss cocksucker motherfucker cunt

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Finales/01 - SELECT/34_sp_baja_logica_micro.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Finales/01 - SELECT/34_sp_baja_logica_micro.sql
@@ -4,8 +4,16 @@ CREATE PROCEDURE [SI_NO_APROBAMOS_HAY_TABLA].sp_baja_logica_micro
 )
 AS
 BEGIN
-	UPDATE [GD1C2013].[SI_NO_APROBAMOS_HAY_TABLA].[Micros]
-	SET Micros.baja = 1
-	WHERE Micros.id_micros = @id_micro
+	SET xact_abort on
+	BEGIN TRANSACTION baja_logica_micro
+		UPDATE [GD1C2013].[SI_NO_APROBAMOS_HAY_TABLA].[Micros]
+		SET Micros.baja = 1
+		WHERE Micros.id_micros = @id_micro
+		
+		UPDATE [GD1C2013].[SI_NO_APROBAMOS_HAY_TABLA].[Butaca]
+		SET Butaca.baja = 1
+		WHERE Butaca.id_micro = @id_micro
+		
+	COMMIT TRANSACTION baja_logica_micro
 END
 
