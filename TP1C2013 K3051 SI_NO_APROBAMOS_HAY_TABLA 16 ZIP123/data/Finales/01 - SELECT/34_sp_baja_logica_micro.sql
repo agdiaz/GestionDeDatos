6ff627cbf44173664b8adb5e9681a3dbd39a2484
demodawid <demodawid@gmail.com>
2013-07-20 05:40:38 -0300

    OJO! Nueva baja logica micro que cancela todas las compras RESUELVE CASO 1 DE DAR DE BAJA UN MICRO

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Finales/01 - SELECT/34_sp_baja_logica_micro.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Finales/01 - SELECT/34_sp_baja_logica_micro.sql
@@ -6,6 +6,39 @@ AS
 BEGIN
 	SET xact_abort on
 	BEGIN TRANSACTION baja_logica_micro
+				
+	DECLARE @idCompraAct int
+
+	DECLARE cur_compras CURSOR LOCAL for
+	SELECT DISTINCT c.id_compra
+	FROM SI_NO_APROBAMOS_HAY_TABLA.Viaje v
+	INNER JOIN SI_NO_APROBAMOS_HAY_TABLA.Pasaje p
+		ON p.id_viaje = v.id_viaje
+	INNER JOIN SI_NO_APROBAMOS_HAY_TABLA.Encomienda e
+		ON e.id_viaje = v.id_viaje
+	INNER JOIN SI_NO_APROBAMOS_HAY_TABLA.Compra c
+		ON p.id_compra = c.id_compra
+		OR e.id_compra = c.id_compra
+	WHERE v.id_micro = @id_micro
+	AND v.baja = 0
+	AND p.baja = 0
+	AND p.fecha_cancel is null
+	AND e.baja = 0
+	AND e.fecha_cancel is null
+
+	open cur_compras
+
+	fetch next from cur_compras into @idCompraAct
+
+	while @@FETCH_STATUS = 0 
+	BEGIN
+		exec SI_NO_APROBAMOS_HAY_TABLA.sp_cancelar_compra @idCompraAct, 'Baja de micro' 
+		fetch next from cur_compras into @idCompraAct 
+	END
+
+		
+		
+		
 		UPDATE [GD1C2013].[SI_NO_APROBAMOS_HAY_TABLA].[Micros]
 		SET Micros.baja = 1
 		WHERE Micros.id_micros = @id_micro
