97df953e3778c4a4dd1660498e23d391ce564763
demodawid <demodawid@gmail.com>
2013-07-20 15:37:06 -0300

    Otra correccion en el maestro

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/ZZ_nuevo_script_maestro.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/ZZ_nuevo_script_maestro.sql
@@ -2354,6 +2354,48 @@ END
 
 GO
 
+CREATE PROCEDURE [SI_NO_APROBAMOS_HAY_TABLA].sp_cancelar_compra
+	@id_compra	int,
+	@motivo		nvarchar(200)
+AS
+BEGIN
+	SET xact_abort on
+	BEGIN TRANSACTION cancel 
+	DECLARE @id_cancel int
+	
+	INSERT INTO SI_NO_APROBAMOS_HAY_TABLA.Cancelacion
+	  (motivo_cancel)
+	VALUES (@motivo)
+	
+	SET @id_cancel = SCOPE_IDENTITY()
+	
+	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Compra
+	SET id_cancelacion = @id_cancel
+	WHERE id_compra = @id_compra
+	
+	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Compra
+	SET baja = 1
+	WHERE id_compra = @id_compra
+	
+	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Encomienda
+	SET id_cancelacion = @id_cancel
+	WHERE id_compra = @id_compra
+	
+	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Encomienda
+	SET baja = 1
+	WHERE id_compra = @id_compra
+	
+	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Pasaje
+	SET id_cancelacion = @id_cancel
+	WHERE id_compra = @id_compra
+	
+	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Pasaje
+	SET baja = 1
+	WHERE id_compra = @id_compra
+	
+	COMMIT TRANSACTION cancel
+END
+GO
 
 CREATE PROCEDURE [SI_NO_APROBAMOS_HAY_TABLA].sp_baja_recorrido
 	@id_recorrido numeric(18,0) 
@@ -2399,48 +2441,6 @@ BEGIN
 	deallocate cur
 
 	COMMIT TRANSACTION baja_recorrido	
-END
-GO
-CREATE PROCEDURE [SI_NO_APROBAMOS_HAY_TABLA].sp_cancelar_compra
-	@id_compra	int,
-	@motivo		nvarchar(200)
-AS
-BEGIN
-	SET xact_abort on
-	BEGIN TRANSACTION cancel 
-	DECLARE @id_cancel int
-	
-	INSERT INTO SI_NO_APROBAMOS_HAY_TABLA.Cancelacion
-	  (motivo_cancel)
-	VALUES (@motivo)
-	
-	SET @id_cancel = SCOPE_IDENTITY()
-	
-	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Compra
-	SET id_cancelacion = @id_cancel
-	WHERE id_compra = @id_compra
-	
-	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Compra
-	SET baja = 1
-	WHERE id_compra = @id_compra
-	
-	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Encomienda
-	SET id_cancelacion = @id_cancel
-	WHERE id_compra = @id_compra
-	
-	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Encomienda
-	SET baja = 1
-	WHERE id_compra = @id_compra
-	
-	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Pasaje
-	SET id_cancelacion = @id_cancel
-	WHERE id_compra = @id_compra
-	
-	UPDATE SI_NO_APROBAMOS_HAY_TABLA.Pasaje
-	SET baja = 1
-	WHERE id_compra = @id_compra
-	
-	COMMIT TRANSACTION cancel
 END
 GO
 
