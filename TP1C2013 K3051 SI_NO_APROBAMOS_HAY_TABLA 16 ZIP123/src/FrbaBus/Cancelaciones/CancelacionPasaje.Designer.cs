f37e55b7c7c7d0c30cb62c9c461768570f065cff
rojascristian <rojas.r.cristian@gmail.com>
2013-07-20 02:28:15 -0300

    Se añaden las pantallas para cancelacion de pasajes y encomiendas

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus/Cancelaciones/CancelacionPasaje.Designer.cs
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/src/FrbaBus/Cancelaciones/CancelacionPasaje.Designer.cs
@@ -28,11 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbCancelacionPasajeMicro = new System.Windows.Forms.TextBox();
+            this.lblCancelacionPasajeMicro = new System.Windows.Forms.Label();
+            this.btnCancelacionPasajeCancelarPasaje = new System.Windows.Forms.Button();
+            this.tbCancelacionPasajeViaje = new System.Windows.Forms.TextBox();
+            this.tbCancelacionPasajePasaje = new System.Windows.Forms.TextBox();
+            this.tbCancelacionPasajeDniCliente = new System.Windows.Forms.TextBox();
+            this.lblCancelacionPasajeViaje = new System.Windows.Forms.Label();
+            this.lblCancelacionPasajePasaje = new System.Windows.Forms.Label();
+            this.lblCancelacionPasajeDniCliente = new System.Windows.Forms.Label();
+            this.btnCancelacionPasajeBuscarCompra = new System.Windows.Forms.Button();
+            this.lblCancelacionPasajeCompra = new System.Windows.Forms.Label();
+            this.tbCancelacionPasajeCompra = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbCancelacionPasajeMicro
+            // 
+            this.tbCancelacionPasajeMicro.Location = new System.Drawing.Point(106, 90);
+            this.tbCancelacionPasajeMicro.Name = "tbCancelacionPasajeMicro";
+            this.tbCancelacionPasajeMicro.Size = new System.Drawing.Size(217, 20);
+            this.tbCancelacionPasajeMicro.TabIndex = 17;
+            // 
+            // lblCancelacionPasajeMicro
+            // 
+            this.lblCancelacionPasajeMicro.AutoSize = true;
+            this.lblCancelacionPasajeMicro.Location = new System.Drawing.Point(11, 93);
+            this.lblCancelacionPasajeMicro.Name = "lblCancelacionPasajeMicro";
+            this.lblCancelacionPasajeMicro.Size = new System.Drawing.Size(33, 13);
+            this.lblCancelacionPasajeMicro.TabIndex = 16;
+            this.lblCancelacionPasajeMicro.Text = "Micro";
+            // 
+            // btnCancelacionPasajeCancelarPasaje
+            // 
+            this.btnCancelacionPasajeCancelarPasaje.Location = new System.Drawing.Point(295, 151);
+            this.btnCancelacionPasajeCancelarPasaje.Name = "btnCancelacionPasajeCancelarPasaje";
+            this.btnCancelacionPasajeCancelarPasaje.Size = new System.Drawing.Size(119, 23);
+            this.btnCancelacionPasajeCancelarPasaje.TabIndex = 15;
+            this.btnCancelacionPasajeCancelarPasaje.Text = "Cancelar Pasaje";
+            this.btnCancelacionPasajeCancelarPasaje.UseVisualStyleBackColor = true;
+            // 
+            // tbCancelacionPasajeViaje
+            // 
+            this.tbCancelacionPasajeViaje.Location = new System.Drawing.Point(106, 64);
+            this.tbCancelacionPasajeViaje.Name = "tbCancelacionPasajeViaje";
+            this.tbCancelacionPasajeViaje.Size = new System.Drawing.Size(217, 20);
+            this.tbCancelacionPasajeViaje.TabIndex = 14;
+            // 
+            // tbCancelacionPasajePasaje
+            // 
+            this.tbCancelacionPasajePasaje.Location = new System.Drawing.Point(106, 116);
+            this.tbCancelacionPasajePasaje.Name = "tbCancelacionPasajePasaje";
+            this.tbCancelacionPasajePasaje.Size = new System.Drawing.Size(217, 20);
+            this.tbCancelacionPasajePasaje.TabIndex = 13;
+            // 
+            // tbCancelacionPasajeDniCliente
+            // 
+            this.tbCancelacionPasajeDniCliente.Location = new System.Drawing.Point(105, 12);
+            this.tbCancelacionPasajeDniCliente.Name = "tbCancelacionPasajeDniCliente";
+            this.tbCancelacionPasajeDniCliente.Size = new System.Drawing.Size(217, 20);
+            this.tbCancelacionPasajeDniCliente.TabIndex = 12;
+            // 
+            // lblCancelacionPasajeViaje
+            // 
+            this.lblCancelacionPasajeViaje.AutoSize = true;
+            this.lblCancelacionPasajeViaje.Location = new System.Drawing.Point(10, 67);
+            this.lblCancelacionPasajeViaje.Name = "lblCancelacionPasajeViaje";
+            this.lblCancelacionPasajeViaje.Size = new System.Drawing.Size(30, 13);
+            this.lblCancelacionPasajeViaje.TabIndex = 11;
+            this.lblCancelacionPasajeViaje.Text = "Viaje";
+            // 
+            // lblCancelacionPasajePasaje
+            // 
+            this.lblCancelacionPasajePasaje.AutoSize = true;
+            this.lblCancelacionPasajePasaje.Location = new System.Drawing.Point(10, 119);
+            this.lblCancelacionPasajePasaje.Name = "lblCancelacionPasajePasaje";
+            this.lblCancelacionPasajePasaje.Size = new System.Drawing.Size(39, 13);
+            this.lblCancelacionPasajePasaje.TabIndex = 10;
+            this.lblCancelacionPasajePasaje.Text = "Pasaje";
+            // 
+            // lblCancelacionPasajeDniCliente
+            // 
+            this.lblCancelacionPasajeDniCliente.AutoSize = true;
+            this.lblCancelacionPasajeDniCliente.Location = new System.Drawing.Point(10, 15);
+            this.lblCancelacionPasajeDniCliente.Name = "lblCancelacionPasajeDniCliente";
+            this.lblCancelacionPasajeDniCliente.Size = new System.Drawing.Size(61, 13);
+            this.lblCancelacionPasajeDniCliente.TabIndex = 9;
+            this.lblCancelacionPasajeDniCliente.Text = "DNI Cliente";
+            // 
+            // btnCancelacionPasajeBuscarCompra
+            // 
+            this.btnCancelacionPasajeBuscarCompra.Location = new System.Drawing.Point(339, 36);
+            this.btnCancelacionPasajeBuscarCompra.Name = "btnCancelacionPasajeBuscarCompra";
+            this.btnCancelacionPasajeBuscarCompra.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelacionPasajeBuscarCompra.TabIndex = 18;
+            this.btnCancelacionPasajeBuscarCompra.Text = "Buscar";
+            this.btnCancelacionPasajeBuscarCompra.UseVisualStyleBackColor = true;
+            // 
+            // lblCancelacionPasajeCompra
+            // 
+            this.lblCancelacionPasajeCompra.AutoSize = true;
+            this.lblCancelacionPasajeCompra.Location = new System.Drawing.Point(10, 41);
+            this.lblCancelacionPasajeCompra.Name = "lblCancelacionPasajeCompra";
+            this.lblCancelacionPasajeCompra.Size = new System.Drawing.Size(43, 13);
+            this.lblCancelacionPasajeCompra.TabIndex = 19;
+            this.lblCancelacionPasajeCompra.Text = "Compra";
+            // 
+            // tbCancelacionPasajeCompra
+            // 
+            this.tbCancelacionPasajeCompra.Location = new System.Drawing.Point(106, 38);
+            this.tbCancelacionPasajeCompra.Name = "tbCancelacionPasajeCompra";
+            this.tbCancelacionPasajeCompra.Size = new System.Drawing.Size(216, 20);
+            this.tbCancelacionPasajeCompra.TabIndex = 20;
+            // 
+            // CancelacionPasaje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "CancelacionPasaje";
+            this.ClientSize = new System.Drawing.Size(426, 185);
+            this.Controls.Add(this.tbCancelacionPasajeCompra);
+            this.Controls.Add(this.lblCancelacionPasajeCompra);
+            this.Controls.Add(this.btnCancelacionPasajeBuscarCompra);
+            this.Controls.Add(this.tbCancelacionPasajeMicro);
+            this.Controls.Add(this.lblCancelacionPasajeMicro);
+            this.Controls.Add(this.btnCancelacionPasajeCancelarPasaje);
+            this.Controls.Add(this.tbCancelacionPasajeViaje);
+            this.Controls.Add(this.tbCancelacionPasajePasaje);
+            this.Controls.Add(this.tbCancelacionPasajeDniCliente);
+            this.Controls.Add(this.lblCancelacionPasajeViaje);
+            this.Controls.Add(this.lblCancelacionPasajePasaje);
+            this.Controls.Add(this.lblCancelacionPasajeDniCliente);
+            this.Name = "CancelacionPasaje";
+            this.Text = "Cancelar Pasaje";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbCancelacionPasajeMicro;
+        private System.Windows.Forms.Label lblCancelacionPasajeMicro;
+        private System.Windows.Forms.Button btnCancelacionPasajeCancelarPasaje;
+        private System.Windows.Forms.TextBox tbCancelacionPasajeViaje;
+        private System.Windows.Forms.TextBox tbCancelacionPasajePasaje;
+        private System.Windows.Forms.TextBox tbCancelacionPasajeDniCliente;
+        private System.Windows.Forms.Label lblCancelacionPasajeViaje;
+        private System.Windows.Forms.Label lblCancelacionPasajePasaje;
+        private System.Windows.Forms.Label lblCancelacionPasajeDniCliente;
+        private System.Windows.Forms.Button btnCancelacionPasajeBuscarCompra;
+        private System.Windows.Forms.Label lblCancelacionPasajeCompra;
+        private System.Windows.Forms.TextBox tbCancelacionPasajeCompra;
+
     }
 }
