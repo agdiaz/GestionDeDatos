9ad8bbf23c93f8a0435e64e8f3256e80fa88ffab
Adrián Diaz <diaz.adrian.g@gmail.com>
2013-07-21 00:50:04 -0300

    generador de scriptsz

---
+++ ZZZ - Varios/Generar/Generar/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Generar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] txtFiles;
+            txtFiles = Directory.GetFiles(textBox1.Text, "*.txt");
+            using (StreamWriter writer = new StreamWriter(textBox1.Text + @"\allfiles.txt"))
+            {
+                for (int i = 0; i < txtFiles.Length; i++)
+                {
+                    using (StreamReader reader = File.OpenText(txtFiles[i]))
+                    {
+                        writer.WriteLine("GO ");
+                        writer.Write(reader.ReadToEnd());
+                        writer.WriteLine("GO ");
+                    }
+                }
+            }
+        }
+    }
+}
