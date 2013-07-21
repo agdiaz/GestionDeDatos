5559f246c15c6de0a187e6b33c749beda19d6081
Adrián Diaz <diaz.adrian.g@gmail.com>
2013-07-21 16:04:58 -0300

    cierro el cursor!

--- TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Finales/01 - SELECT/81_sp_procesar_arribos.sql
+++ TP1C2013 K3051 SI_NO_APROBAMOS_HAY_TABLA 16 ZIP123/data/Finales/01 - SELECT/81_sp_procesar_arribos.sql
@@ -7,19 +7,26 @@ BEGIN
 	DECLARE Cursor_arribos Cursor FOR
 		SELECT Id_viaje, Fecha_arribo_real
 		FROM SI_NO_APROBAMOS_HAY_TABLA.ArribosTemporal
+	
+	OPEN Cursor_arribos
+	
 	BEGIN TRY
 		BEGIN TRAN
-			OPEN Cursor_arribos
+			
 			Fetch NEXT FROM Cursor_arribos INTO @id_viaje, @fecha_arribo_real
 			While (@@FETCH_STATUS <> -1)
 			BEGIN
 				UPDATE SI_NO_APROBAMOS_HAY_TABLA.Viaje
 				SET fecha_arribo = @fecha_arribo_real
 				WHERE id_viaje = @id_viaje	
+				
+				FETCH NEXT FROM Cursor_arribos INTO @id_viaje, @fecha_arribo_real
 			END 
 		COMMIT
 	END TRY
 	BEGIN CATCH 
 		ROLLBACK
 	END CATCH	
+	CLOSE Cursor_arribos
+	DEALLOCATE Cursor_arribos
 END
